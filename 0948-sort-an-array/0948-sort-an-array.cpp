9f3e6797c262923de45ce29be7b3248953d76303
samim <samim2356sk@gmail.com>
2026-08-28 10:28:32 +0530

    0948-SORT-AN-ARRAY Stats: Time: 40 ms (84.49%), Space: 71 MB (87.43%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -4,34 +4,27 @@ private:
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-
         if (left < n && nums[left] > nums[largest]) {
             largest = left;
         }
-
         if (right < n && nums[right] > nums[largest]) {
             largest = right;
         }
-
         if (largest != i) {
             swap(nums[i], nums[largest]);
             heapify(nums, n, largest);
         }
     }
-
 public:
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
-
         for (int i = n / 2 - 1; i >= 0; --i) {
             heapify(nums, n, i);
         }
-
         for (int i = n - 1; i > 0; --i) {
             swap(nums[0], nums[i]);
             heapify(nums, i, 0);
         }
-
         return nums;
     }
 };
